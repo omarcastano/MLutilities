7e9f98e0ffb4713e0a14fded549889fdea3b8d1b
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-05-24 22:11:48 -0500

    Create metrics.py

---
+++ Classification/metrics.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, average_precision_score
+import pandas as pd
+import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.preprocessing import LabelEncoder
+import scipy.stats as stats
+import numpy as np
