2e91ab03b33cca139c2f7d1a89eae25ee907eb0b
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-06-13 22:14:38 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ML Utilities'
+    version='0.0.1-dev',
+    author='Omar Andres Castaño',
+    description='Machine Learning Utilities',
+    long_description='This packages provides a series of machine learning utilities which makes easy to teach machine learning topics',
+    url='https://github.com/omarcastano/MLutilities.git',
+    python_requires='>=3.7, <4',
+    packages=find_packages()
+)
