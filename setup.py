d9b538b8d1f2313c5ca42d3e344b79919c7ec69a
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-06-13 22:53:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     description='Machine Learning Utilities',
     long_description='This packages provides a series of machine learning utilities which makes easy to teach machine learning topics',
     url='https://github.com/omarcastano/MLutilities.git',
-    packages=find_packages(),
-    install_requires=['wheel', 'bar', 'greek']
+    packages=find_packages()
 )
