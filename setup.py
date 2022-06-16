4aa58e9ca4b9d1245116f8140a2e68bae440e489
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-06-15 23:46:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     long_description='This packages provides a series of machine learning utilities which makes easy to teach machine learning topics',
     url='https://github.com/omarcastano/MLutilities.git',
     packages=find_packages()
+    package_data = {'MLutilities':['datasets/diamonds.pkl']}
 )
