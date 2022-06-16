05e5ed7074b9da5f595fe0e5a9c65307339d4201
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-06-15 23:31:05 -0500

    Update load_data.py

--- MLutilities/datasets/load_data.py
+++ MLutilities/datasets/load_data.py
@@ -6,6 +6,6 @@ def diamonds():
   """
   Load Diamonds dataset
   """
-  a_file = open('MLutilities/datasets/diamonds.pkl', "rb")
+  a_file = open('./MLutilities/datasets/diamonds.pkl', "rb")
   
   return pickle.load(a_file)
