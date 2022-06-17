7e0b8a4588f3c92d1ac777eb3826298d000cbc04
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-06-17 08:30:38 -0500

    Update load_data.py

--- MLutilities/datasets/load_data.py
+++ MLutilities/datasets/load_data.py
@@ -1,15 +1,35 @@
 import pickle
-
+import pandas as pd
+import numpy as np
 import pkg_resources
 
 
-def diamonds():
+def diamonds(load_as:str='list'):
   
-  """
-  Load Diamonds dataset
-  """
+    """
+    Returns a dicctionary that loads Diamonds dataset. The dicctionary has 
+    the follow keys:
+    DESC: a breve description of the dataset and its columns
+    data: raw data (rows and columns)
+    feature_names: the name of the features/columns
+
+    Argumetns:
+        load_as: str. possible options: 'dict', 'list or 'numpy'
+            this arguments contros how the raw data is stored.
+            * 'dict' : dict like {column -> [values]}
+            * 'list' : list like [[column 1],[column 2],[column 3]]
+            * 'numpy' : numpy like numpy.array([[column 1],[column 2],[column 3]])
+    """
+
+    assert load_as in ['dict', 'list', 'numpy'], "load_as mus be on of the possible options: 'dict', 'list or 'numpy'"
+
+    path_to_data = pkg_resources.resource_filename(__name__, 'diamonds.pkl')
+    a_file = open(path_to_data, "rb")
+    data = pickle.load(a_file)
   
-  path_to_data = pkg_resources.resource_filename(__name__, 'diamonds.pkl')
-  a_file = open(path_to_data, "rb")
-  
-  return pickle.load(a_file)
+    if load_as=='dict':
+        return {'DESC':description, 'data':data.to_dict(orient='list'), 'feature_names':df.columns}
+    elif load_as=='list':
+        return {'DESC':description, 'data':data.to_numpy().tolist(), 'feature_names':df.columns}
+    elif load_as=='numpy':
+        return {'DESC':description, 'data':data.to_numpy(), 'feature_names':df.columns}
