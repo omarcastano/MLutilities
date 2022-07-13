c6c7b6e6c3ecd9e4570949c7dbee5d3588422a75
omarcastano <omar.castano@udea.edu.co>
2022-07-13 14:56:35 -0500

    example histogram

--- MLutilities/stats/distributions.py
+++ MLutilities/stats/distributions.py
@@ -22,16 +22,20 @@ def example_histogram(mean:float=0.0, std:float=1.0, alpha:float=0.0):
 
     fig , ax = plt.subplots(2, 2, figsize=(28,8)) 
 
-    ax[1,0].hist(normal, bins=50, edgecolor='k', label=f"mean={normal.mean().round(2)}\n" +  
-                                                        f"std={normal.std().round(2)}\n" +
+    ax[1,0].hist(normal, bins=50, edgecolor='k', label=f"std={normal.std().round(2)}\n" +
                                                         f"skewness={round(stats.skew(normal), 2)}\n" +
                                                         f"kurtosis={round(stats.kurtosis(normal), 2)}")
-
-    ax[1,1].hist(rvs, bins=50, edgecolor='k', label=f"mean={rvs.mean().round(2)}\n" +  
-                                                        f"std={rvs.std().round(2)}\n" +
+    
+    ax[1,0].vlines(np.mean(normal), 0, 3800, color='r', label=f'Mean={round(np.mean(normal),2)}', lw=5)
+    ax[1,0].vlines(np.median(normal), 0, 3800, color='k', label=f'Median={round(np.median(normal), 2)}', lw=5)
+    
+    ax[1,1].hist(rvs, bins=50, edgecolor='k', label=f"std={rvs.std().round(2)}\n" +
                                                         f"skewness={round(stats.skew(rvs), 2)}\n" +
                                                         f"kurtosis={round(stats.kurtosis(rvs), 2)}")
-
+    
+    ax[1,1].vlines(np.mean(rvs), 0, 3800, color='r', label=f'Mean={round(np.mean(rvs),2)}', lw=5)
+    ax[1,1].vlines(np.median(rvs), 0, 3800, color='k', label=f'Median={round(np.median(rvs), 2)}', lw=5)
+        
     ax[0,1].boxplot(rvs, patch_artist=True, vert=False) 
     ax[0,0].boxplot(normal, patch_artist=True, vert=False)
 
