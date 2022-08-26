bb2a8d7bab2df3416b90423c3ede62bdcccbe7d6
omarcastano <omar.castano@udea.edu.co>
2022-08-25 20:06:56 -0500

    added countplot

--- MLutilities/EDA/widgets.py
+++ MLutilities/EDA/widgets.py
@@ -4,6 +4,7 @@ import numpy as np
 from functools import partial
 from MLutilities.EDA import kolmogorov_test, correlation_coef
 from IPython.display import display
+import plotly.express as px
 
 
 def kolmogorov_test_widget(dataset: pd.DataFrame):
@@ -95,7 +96,7 @@ def correlation_coef_widget(dataset: pd.DataFrame):
     )
     kind = widgets.Dropdown(
         options=["pearson", "spearman", "kendall"],
-        description="Transformation:",
+        description="Method:",
         layout=widgets.Layout(width="20%", height="30px"),
         style={"description_width": "initial"},
     )
@@ -110,3 +111,37 @@ def correlation_coef_widget(dataset: pd.DataFrame):
     )
 
     display(widgets.HBox([variable1, variable2, kind]), w)
+
+
+def countplot_widget(dataset: pd.DataFrame):
+
+    """
+    Show the counts of observations in each categorical bin using bars. A count plot can be
+    thought of as a histogram across a categorical, instead of quantitative variable.
+    This function will infer data types, so it is highly recomended to set categorical variables
+    as string or pd.Categorical
+
+    Arguments:
+    ---------
+        dataset: pandas dataframe or dict with de format {'col1':np.array, 'col2':np.array}
+
+    """
+
+    num_vars = dataset.select_dtypes([np.number]).columns
+    cat_vars = dataset.select_dtypes([object, pd.Categorical]).columns.tolist()
+
+    variable = widgets.Dropdown(
+        options=cat_vars,
+        description="Variable:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+
+    def hist(dataset, **kwargs):
+
+        fig = px.histogram(data_frame=dataset, **kwargs)
+        fig.update_layout(width=1500, height=500)
+        fig.show()
+
+    w = widgets.interactive_output(partial(hist, dataset=dataset), {"x": variable})
+    display(variable, w)
