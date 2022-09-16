e13061ed7a24430e66d78a750c53fa8860ff3c2a
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-09-16 16:24:09 -0500

    adding regularization viz

---
+++ MLutilities/Regression/widgets.py
@@ -0,0 +1,86 @@
+from MLutilities.utils import plot_polyreg
+from functools import partial
+import ipywidgets as widgets
+
+
+def regularization_widget():
+    """
+    this function helps to visualize the effect of regularization in a regression model
+    """
+    N = widgets.IntSlider(
+        description="Number of points",
+        min=50,
+        max=500,
+        value=50,
+        continuous_update=False,
+        layout=widgets.Layout(width="20%", height="50px"),
+        style={"description_width": "initial"},
+    )
+
+    degree = widgets.IntSlider(
+        description="Polynomial degree",
+        min=1,
+        max=15,
+        value=2,
+        continuous_update=False,
+        layout=widgets.Layout(width="20%", height="50px"),
+        style={"description_width": "initial"},
+    )
+
+    estimator = widgets.Dropdown(
+        options=["LinearRegression", "Ridge", "Lasso", "ElasticNet"],
+        description="Estimator:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+
+    alpha = widgets.FloatSlider(
+        description="Alpha",
+        min=0.01,
+        max=10,
+        value=1,
+        step=0.01,
+        continuous_update=False,
+        layout=widgets.Layout(width="20%", height="50px"),
+        style={"description_width": "initial"},
+    )
+
+    l1_ratio = widgets.FloatSlider(
+        description="l1_ratio",
+        min=0.1,
+        max=1,
+        value=0.5,
+        continuous_update=False,
+        layout=widgets.Layout(width="20%", height="50px"),
+        style={"description_width": "initial"},
+    )
+    helper_viz = widgets.Dropdown(
+        options=["learning curve", "weights"],
+        value="weights",
+        description="Second viz:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+
+    w = widgets.interactive_output(
+        partial(plot_polyreg),
+        {
+            "N": N,
+            "degree": degree,
+            "estimator": estimator,
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+            "helper_viz": helper_viz,
+        },
+    )
+
+    display(
+        widgets.VBox(
+            [
+                widgets.HBox([estimator, helper_viz]),
+                widgets.HBox([N, degree]),
+                widgets.HBox([alpha, l1_ratio]),
+            ]
+        ),
+        w,
+    )
--- MLutilities/utils.py
+++ MLutilities/utils.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import List
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from yellowbrick.model_selection import LearningCurve
 
 
 def cramerv_relationship_strength(degrees_of_freedom, cramerv):
@@ -58,3 +62,106 @@ def scaler(
     ax[1].set_title("Transform")
     ax[0].legend()
     ax[1].legend()
+
+
+sns.set()
+
+
+def generate_nonlinear_data(N, seed=1):
+    """
+    generate (x, y) instances with a non-linear relationship
+
+    Arguments:
+    ----------
+    N (int):     number of instances
+    seed (int):  seed for the random numbers generator
+    """
+    np.random.seed(seed)
+    X = np.random.rand(N) ** 2
+    y = 10 - 1 / (X + 0.1) + 2 * np.random.rand(N)
+    return X.reshape(-1, 1), y
+
+
+def poly_reg(estimator, degree=2):
+    """
+    polinomial regression estimator
+
+    Arguments:
+    ----------
+    estimator (sklearn estimator): estimator to include in the model pipeline
+    degree (int):                  degree of the PolynomialFeatures transformation
+    """
+    poly = PolynomialFeatures(degree=degree, include_bias=False)
+    scaler = StandardScaler()
+    return make_pipeline(poly, scaler, estimator)
+
+
+def plot_learning_curve(linear_estimator, X, y, degree, ax):
+    visualizer = LearningCurve(
+        estimator=poly_reg(linear_estimator, degree),
+        scoring="r2",
+        train_sizes=np.linspace(0.2, 1.0, 12),
+        ax=ax,
+    )
+
+    visualizer.fit(X, y)
+    ax.set_ylim(0, 1.1)
+    visualizer.show()
+
+
+def plot_polyreg(
+    N=50, estimator="LinearRegression", degree=2, alpha=1, l1_ratio=1, helper_viz=False
+):
+    """
+    plot a polynomial regression model with and a helper visualization
+
+    Arguments:
+    --------
+    N:           number of instances
+    degree:      degree to use in a PolynomialFeatures transformation
+    estimator:   estimator to use in the model pipeline (LinearRegression, Ridge, Lasso and ElasticNet)
+    alpha:       regularization parameter for Ridge, Lasso and ElasticNet estimators
+    l1_ration:   regularization parameter for the ElasticNet estimator
+    helper_viz:  helper visualization (weights plot or Learning Curve
+    """
+    # train and test data
+    X, y = generate_nonlinear_data(N=N, seed=1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1
+    )
+
+    # model training
+    estimators = {
+        "LinearRegression": LinearRegression(),
+        "Ridge": Ridge(alpha=alpha),
+        "Lasso": Lasso(alpha=alpha),
+        "ElasticNet": ElasticNet(alpha=alpha, l1_ratio=l1_ratio),
+    }
+    model = poly_reg(estimators[estimator], degree)
+    model.fit(X_train, y_train)
+
+    # predictions
+    x_pred = np.linspace(0, 1, 100).reshape(-1, 1)
+    y_pred = model.predict(x_pred)
+
+    # visualization
+    fig, ax = plt.subplots(1, 2, figsize=(25, 8))
+
+    # train and validation scores
+    train_score = model.score(X_train, y_train)
+    val_score = model.score(X_test, y_test)
+    ax[0].scatter(X_train, y_train, label=f"train score: {train_score:.2f}")
+    ax[0].scatter(X_test, y_test, label=f"validation score: {val_score:.2f}")
+
+    # model predictions
+    ax[0].plot(x_pred, y_pred, "r", label="Model")
+
+    # helper visualization
+    if helper_viz == "learning curve":
+        plot_learning_curve(estimators[estimator], X, y, degree, ax[1])
+    elif helper_viz == "weights":
+        ax[1].plot(model[f"{estimator.lower()}"].coef_, "ro--")
+        ax[1].set(xlabel="Weight", ylabel="Value")
+
+    ax[0].set_title(f"Estimator: {estimator}\nDegree: {degree}")
+    ax[0].legend()
