7f7c24338102ac5c82799404682b41fe21980585
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-09-22 18:23:43 -0500

    adding plot_log_reg helper viz

--- MLutilities/utils.py
+++ MLutilities/utils.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import List, Tuple
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
+from sklearn.linear_model import (
+    LinearRegression,
+    Ridge,
+    Lasso,
+    ElasticNet,
+    LogisticRegression,
+)
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 from yellowbrick.model_selection import LearningCurve
 
 
@@ -18,7 +24,8 @@ def cramerv_relationship_strength(degrees_of_freedom: int, cramerv: float):
     Arguments:
     ----------
     degrees_of_freedom:  degrees of freedom obtained from a contingency
-                         table as: min(n_rows - 1, n_cols - 1)
+                         table as:
+                            min(number of rows - 1, number of columns - 1)
     cramerv:             Cramer's V coefficient
     """
     values = {
@@ -47,9 +54,10 @@ def scaler(
     Helper function to visualize the efect of scaling and normalization over continuos variables
 
     Arguments:
-        dataset: pandas dataframe or dict with the format {'col1':np.array, 'col2':np.array}
-        varaibles: list with the name of the features to scale
-        kind: name of the transformation to perform. Options ["standar_scaler", "minmax"]
+    ----------
+    dataset:   pandas dataframe or dict with the format {'col1':np.array, 'col2':np.array}
+    variables: list with the name of the features to scale
+    kind:      name of the transformation to perform. Options ["standar_scaler", "minmax"]
     """
 
     scale = {
@@ -84,14 +92,14 @@ def generate_nonlinear_data(N: int, seed: int = 1) -> Tuple:
     return X.reshape(-1, 1), y
 
 
-def poly_reg(estimator, degree=2):
+def poly_reg(estimator, degree: int = 2):
     """
     returns a polinomial regression estimator
 
     Arguments:
     ----------
     estimator (sklearn estimator): estimator to include in the model pipeline
-    degree (int):                  degree of the PolynomialFeatures transformation
+    degree:                        degree of the PolynomialFeatures transformation
     """
     poly = PolynomialFeatures(degree=degree, include_bias=False)
     scaler = StandardScaler()
@@ -121,10 +129,10 @@ def plot_polyreg(
     --------
     N:           number of instances
     degree:      degree to use in a PolynomialFeatures transformation
-    estimator:   estimator to use in the model pipeline (LinearRegression, Ridge, Lasso and ElasticNet)
+    estimator:   estimator to use in the model pipeline ("LinearRegression", "Ridge", "Lasso" or "ElasticNet")
     alpha:       regularization parameter for Ridge, Lasso and ElasticNet estimators
     l1_ration:   regularization parameter for the ElasticNet estimator
-    helper_viz:  helper visualization (weights plot or Learning Curve
+    helper_viz:  helper visualization (weights plot or Learning Curve)
     """
     # train and test data
     X, y = generate_nonlinear_data(N=N, seed=1)
@@ -167,3 +175,95 @@ def plot_polyreg(
 
     ax[0].set_title(f"Estimator: {estimator}\nDegree: {degree}")
     ax[0].legend()
+
+
+def plot_log_reg(
+    threshold: float = 0.5,
+    regression: str = "none",
+    point_position: int = 12,
+) -> None:
+    """
+    helper visualization to illustrate linear and logistic regressions in a 1D binary classification problem
+
+    Arguments:
+    ----------
+    threshold:      threshold for the decision region
+    regression:     regression to use ("none", "linear", "logistic")
+    point_position: position of a positive class point
+    """
+    n_instances_negative_class = 6
+    n_instances_positive_class = 12
+    start = 1
+    end = 12
+    classes_intercept = 5
+
+    instances_negative_class = np.linspace(
+        start, classes_intercept + 1, n_instances_negative_class
+    )
+    instances_positive_class = np.concatenate(
+        [np.linspace(classes_intercept, end, n_instances_positive_class)],
+        [point_position],
+    )
+    data = np.concatenate([instances_negative_class, instances_positive_class])
+
+    labels_negative_class = np.zeros_like(instances_negative_class)
+    labels_positive_class = np.ones_like(instances_positive_class)
+    labels = np.concatenate([labels_negative_class, labels_positive_class])
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(x=data, y=labels, c=labels, cmap="coolwarm")
+    ax.set(ylabel="Label", xlabel="Feature")
+
+    if regression != "none":
+        # model training
+        estimators = {
+            "linear": LinearRegression(),
+            "logistic": LogisticRegression(),
+        }
+        model = estimators[regression]
+        model.fit(data.reshape(-1, 1), labels)
+
+        # predictions and decision frontier
+        x = np.linspace(start, point_position)
+
+        if regression == "linear":
+            decision_frontier = (threshold - model.intercept_) / model.coef_[0]
+            y_pred = model.predict(x.reshape(-1, 1))
+            y_label = "$y=w_0 + w_1 x_1$"
+        elif regression == "logistic":
+            decision_frontier = (-1 / model.coef_[0]) * (
+                model.intercept_ + np.log(1 / threshold - 1)
+            )
+            y_pred = model.predict_proba(x.reshape(-1, 1))[:, 1]
+            y_label = "$\hat P (C_1 | \textbf{x})$"
+
+        ymin = min(y_pred) if min(y_pred) <= 0 else 0
+        sns.lineplot(x=x, y=y_pred, color="k", label=y_label)
+
+        # decision regions
+        if not decision_frontier <= 0:
+            ax.fill_betweenx(
+                y=np.linspace(ymin, max(y_pred)),
+                x1=start,
+                x2=decision_frontier,
+                alpha=0.2,
+                color="b",
+            )
+        ax.fill_betweenx(
+            y=np.linspace(ymin, max(y_pred)),
+            x1=decision_frontier,
+            x2=point_position,
+            alpha=0.2,
+            color="r",
+        )
+
+        # threshold line
+        ax.hlines(
+            threshold,
+            start,
+            point_position,
+            linestyle="--",
+            alpha=0.5,
+            label=f"Threshold",
+        )
+        ax.legend(fontsize=15)
