47e045b6c637208a461e4cf6d2a76742018851e3
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-09-22 18:27:15 -0500

    adding logistic regression widget

---
+++ MLutilities/Classification/classification_widgets.py
@@ -0,0 +1,46 @@
+import ipywidgets as widgets
+from functools import partial
+from IPython.display import display
+from MLutilities.utils import plot_log_reg
+
+
+def logistic_regression_widget():
+    threshold = widgets.FloatSlider(
+        description="Threshold",
+        min=0.01,
+        max=0.99,
+        value=0.5,
+        step=0.01,
+        continuous_update=False,
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+
+    regression = widgets.Dropdown(
+        options=["none", "linear", "logistic"],
+        description="Regression:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+
+    outlier_pos = widgets.IntSlider(
+        description="Outlier position",
+        min=12,
+        max=40,
+        value=12,
+        step=4,
+        continuous_update=False,
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+
+    w = widgets.interactive_output(
+        partial(plot_log_reg),
+        {
+            "threshold": threshold,
+            "regression": regression,
+            "outlier_pos": outlier_pos,
+        },
+    )
+
+    display(widgets.VBox([regression, threshold, outlier_pos]), w)
