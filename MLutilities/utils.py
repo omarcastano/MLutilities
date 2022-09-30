bb010b5412db872b49c197dc13d8cb0eaa1bfbae
Daniel Ocampo Henao <daniel.ocampoh@udea.edu.co>
2022-09-30 12:29:34 -0500

    adding get_metrics_data func

--- MLutilities/utils.py
+++ MLutilities/utils.py
@@ -315,8 +315,111 @@ def highlight_quadrants(metric: str, ax=None):
     """
     highlight confusion matrix quadrants associated with a metric
     """
-    with open("MLutilities/data/metrics_data.pkl", "r") as f:
-        metrics_data = pickle.load(f)
+    metrics_data = get_metrics_data()
     quadrant = metrics_data[metric]["quadrant"]
     for i in quadrant:
         highlight_quadrant(quadrant=i, ax=ax)
+       
+def get_metrics_data() -> dict:
+    """
+    returns a dictionary with metrics functions, formulas and confusion matrix quadrants
+    """
+    metrics_keys = [
+        "accuracy",
+        "precision",
+        "recall",
+        "f1_score",
+        "TPR",
+        "TNR",
+        "FNR",
+        "FPR",
+        "NPV",
+    ]
+
+    # functions to compute each metric in terms of the confusion matrix entries
+    def accuracy(tn, fp, fn, tp):
+        return (tp + tn) / (tp + fp + tn + fn + 10e-8)
+
+
+    def precision(tn, fp, fn, tp):
+        return tp / (tp + fp + 10e-8)
+
+
+    def recall(tn, fp, fn, tp):
+        return tp / (tp + fn + 10e-8)
+
+
+    def f1_score(tn, fp, fn, tp):
+        return (
+            2
+            * ((tp / (tp + fp + 10e-8)) * (tp / (tp + fn + 10e-8)))
+            / ((tp / (tp + fp)) + (tp / (tp + fn)))
+        )
+
+
+    def tpr(tn, fp, fn, tp):
+        return tp / (tp + fn + 10e-8)
+
+
+    def tnr(tn, fp, fn, tp):
+        return tn / (tn + fp + 10e-8)
+
+
+    def fnr(tn, fp, fn, tp):
+        return fn / (fn + tp + 10e-8)
+
+
+    def fpr(tn, fp, fn, tp):
+        return fp / (fp + tn + 10e-8)
+
+
+    def npv(tn, fp, fn, tp):
+        return tn / (tn + fn + 10e-8)
+
+
+    metrics_functions = [
+        accuracy,
+        precision,
+        recall,
+        f1_score,
+        tpr,
+        tnr,
+        fnr,
+        fpr,
+        npv,
+    ]
+
+    # confusion matrix quadrants asociated with each metric
+    metric_quadrants = [
+        [0, 1, 2, 3],
+        [1, 3],
+        [3, 2],
+        [3, 1, 2],
+        [3, 2],
+        [0, 1],
+        [3, 2],
+        [0, 1],
+        [0, 2],
+    ]
+
+    # mathematical expression for each metric
+    metrics_formulas = [
+        r"$\frac{TP + {TN}}{{TP}+{TN}+{FP}+{FN}}$",
+        r"$\frac{TP}{TP + FP}$",
+        r"$\frac{TP}{TP+FN}$",
+        r"$2 \frac{P*R}{P+R}$",
+        r"$\frac{TP}{TP+FN}$",
+        r"$\frac{TN}{TN + FP}$",
+        r"$\frac{FN}{FN + TP}$",
+        r"$\frac{FP}{FP + TN}$",
+        r"$\frac{TN}{TN + FN}$",
+    ]
+
+    metrics_data = {}
+    for i, key in enumerate(metrics_keys):
+        metrics_data[key] = {}
+        metrics_data[key]["function"] = metrics_functions[i]
+        metrics_data[key]["formula"] = metrics_formulas[i]
+        metrics_data[key]["quadrant"] = metric_quadrants[i]
+
+    return metrics_data
