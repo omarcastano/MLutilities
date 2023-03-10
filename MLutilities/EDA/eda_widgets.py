7cf8def5454c3bb50f28717745032973f5904986
Daniel Ocampo-Henao <daniel.ocampoh@udea.edu.co>
2023-03-10 13:55:10 -0500

    add kde_widget

--- MLutilities/EDA/eda_widgets.py
+++ MLutilities/EDA/eda_widgets.py
@@ -3,13 +3,20 @@ import pandas as pd
 import plotly.express as px
 import ipywidgets as widgets
 from functools import partial
-from MLutilities.utils import scaler
-from MLutilities.EDA import kolmogorov_test, shapiro_test, correlation_coef, kruskal_test, cramersv
 from IPython.display import display
+from typing import Union, Dict
+from MLutilities.utils import scaler
+from MLutilities.EDA import (
+    kolmogorov_test,
+    shapiro_test,
+    correlation_coef,
+    kruskal_test,
+    cramersv,
+    kde_plot,
+)
 
 
 def kolmogorov_test_widget(dataset: pd.DataFrame):
-
     """
     This function computes Kolmogorov test to check if the variable
     is normaly distributed
@@ -68,7 +75,6 @@ def kolmogorov_test_widget(dataset: pd.DataFrame):
 
 
 def shapiro_test_widget(dataset: pd.DataFrame):
-
     """
     This function computes Shapiro test to check if the variable
     is normaly distributed
@@ -127,7 +133,6 @@ def shapiro_test_widget(dataset: pd.DataFrame):
 
 
 def correlation_coef_widget(dataset: pd.DataFrame):
-
     """
     This function computes the correlation between two numerical variables.
 
@@ -180,7 +185,6 @@ def correlation_coef_widget(dataset: pd.DataFrame):
 
 
 def countplot_widget(dataset: pd.DataFrame):
-
     """
     Show the counts of observations in each categorical bin using bars. A count plot can be
     thought of as a histogram across a categorical, instead of quantitative variable.
@@ -207,17 +211,17 @@ def countplot_widget(dataset: pd.DataFrame):
     )
 
     def hist(dataset, **kwargs):
-
         fig = px.histogram(data_frame=dataset, barmode="group", **kwargs)
         fig.update_layout(width=1500, height=500)
         fig.show()
 
-    w = widgets.interactive_output(partial(hist, dataset=dataset), {"x": variable, "color": color})
+    w = widgets.interactive_output(
+        partial(hist, dataset=dataset), {"x": variable, "color": color}
+    )
     display(widgets.HBox([variable, color]), w)
 
 
 def kruskal_test_widget(dataset: pd.DataFrame):
-
     """
     The Kruskal-Wallis H test is a rank-based nonparametric test
     that can be used to determine if there are statistically significant
@@ -262,7 +266,6 @@ def kruskal_test_widget(dataset: pd.DataFrame):
 
 
 def barplot_widget(dataset: pd.DataFrame):
-
     """
     A bar plot represents an estimate of central tendency for a numeric variable with the height
     of each rectangle and provides some indication of the uncertainty around that estimate using
@@ -298,7 +301,6 @@ def barplot_widget(dataset: pd.DataFrame):
     )
 
     def barplot(dataset, cat_var=None, num_var=None, func="mean"):
-
         df = dataset.groupby(cat_var, as_index=False).agg({f"{num_var}": [func]})
         df.columns = ["_".join(col) for col in df.columns.values]
 
@@ -318,7 +320,6 @@ def barplot_widget(dataset: pd.DataFrame):
 
 
 def cramerv_widget(dataset: pd.DataFrame):
-
     """
     This function computes cramer's V correlation coefficient which is a measure of association between two nominal variables.
 
@@ -365,7 +366,6 @@ def cramerv_widget(dataset: pd.DataFrame):
 
 
 def scaler_widget(dataset: pd.DataFrame):
-
     """
     Helper function to visualize the efect of scaling and normalization over continuos variables
 
@@ -397,3 +397,51 @@ def scaler_widget(dataset: pd.DataFrame):
     )
 
     display(widgets.HBox([num_variable, kind]), w)
+
+
+def kde_widget(dataset: Union[pd.DataFrame, Dict[str, np.ndarray]]):
+    """
+    Generate a interactive widget for a kernel density estimate (KDE) plot for a given variable in the dataset. Optionally applies a
+    transformation to the variable before generating the plot.
+
+    Args:
+        dataset (pd.DataFrame or dict with format {'col1': np.array, 'col2': np.array}): The input dataset to use
+            for generating the KDE plot.
+    """
+    num_vars = dataset.select_dtypes([np.number]).columns
+    cat_vars = dataset.select_dtypes([object, "category"]).columns.tolist()
+
+    variable = widgets.Dropdown(
+        options=num_vars,
+        description="Variable:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+    transformation = widgets.Dropdown(
+        options=["None", "yeo_johnson", "log"],
+        description="Transformation:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+    color = widgets.Dropdown(
+        options=[None] + cat_vars,
+        description="Color:",
+        layout=widgets.Layout(width="20%", height="30px"),
+        style={"description_width": "initial"},
+    )
+    plot_boxplot = widgets.Checkbox(
+        value=False,
+        description="boxplot",
+        layout=widgets.Layout(width="5%", height="5px"),
+        style={"description_width": "initial"},
+    )
+    w = widgets.interactive_output(
+        partial(kde_plot, dataset=dataset),
+        {
+            "variable": variable,
+            "transformation": transformation,
+            "color": color,
+            "plot_boxplot": plot_boxplot,
+        },
+    )
+    display(widgets.HBox([variable, color, transformation, plot_boxplot]), w)
